972f7a1c24ca22fc04bbd68df532d494da58fecc
IlyaKubyshkin <ilkubik@yandex.ru>
2018-07-26 13:42:37 +0300

    it dosn't work

--- app/src/main/java/com/example/kubik/testviewpagertask/MainActivity.kt
+++ app/src/main/java/com/example/kubik/testviewpagertask/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kubik.testviewpagertask
 
 import android.os.Bundle
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,15 +19,33 @@ class MainActivity : AppCompatActivity() {
             override fun getCount() = 3
         }
 
-        viewPager.setPageTransformer(true) { view, position ->
+      /*  viewPager.setPageTransformer(true) { view, position ->
            // textView.alpha = 1.0f - Math.abs(position)
 
-            textView.text = StringBuffer("This page's number is: ").append(position).toString()
+            setText(position.toString() + "   :  " + viewPager.currentItem)
+        }*/
 
-            Log.d("myLogs", "position:  " + position.toString())
-            Log.d("myLogs", "current item:  " + viewPager.currentItem.toString())
-        }
+        setText(viewPager.currentItem.toString())
 
         tabLayout.setupWithViewPager(viewPager, true)
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                setText(positionOffset.toString() + "    :   " + position.toString())
+
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+        })
+    }
+
+    private fun setText(text: String) {
+        textView.text = StringBuffer("This page's number is: ").append(text).toString()
     }
 }
